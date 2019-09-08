40af7f9d48b1d77cc4c0a5d0f2272a7e2cf250df
Omar El Sergany <omar.m.zaki@gmail.com>
2019-09-08 21:02:29 +0200

    Umbraco Page Views Counter

---
+++ Reflections.Umbraco.Utilities/PageViewCounter/PageViewCounter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Web.Configuration;
+
+namespace Reflections.UmbracoUtilities
+{
+    public static class PageViewCounter 
+    {
+        public static void SetPageViewCount(int nodeId)
+        {
+            using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["umbracoDbDSN"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("ReflectionsUmbracoUtilitiesSetPageViewCount", conn))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.Add("NodeId", System.Data.SqlDbType.Int).Value = nodeId;
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static int GetPageViewCount(int nodeId)
+        {
+            int viewCount = 0;
+            using (SqlConnection conn = new SqlConnection(WebConfigurationManager.ConnectionStrings["umbracoDbDSN"].ConnectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("ReflectionsUmbracoUtilitiesGetPageViewCount", conn))
+                {
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.Add("NodeId", System.Data.SqlDbType.Int).Value = nodeId;
+                    conn.Open();
+                    viewCount = (int)cmd.ExecuteScalar();
+                }
+            }
+            return viewCount;
+        }
+
+    }
+
+   
+}
---
+++ Reflections.Umbraco.Utilities/PageViewCounter/Script.sql
@@ -0,0 +1,99 @@
+﻿SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ReflectionsUmbracoUtilitiesPageViewCounter](
+ [NodeId] [int] NOT NULL,
+ [ViewsCounter] [int] NOT NULL,
+ CONSTRAINT [PK_ReflectionsUmbracoUtilitiesPageViewCounter] PRIMARY KEY CLUSTERED 
+(
+ [NodeId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+SET ANSI_NULLS ON
+
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[ReflectionsUmbracoUtilitiesSetPageViewCount]
+(
+    @NodeId INT
+)
+AS
+BEGIN
+    SET NOCOUNT ON;
+    IF EXISTS(SELECT 1 FROM ReflectionsUmbracoUtilitiesPageViewCounter WHERE NodeId = @NodeId)
+    BEGIN
+        UPDATE ReflectionsUmbracoUtilitiesPageViewCounter SET ViewsCounter = ViewsCounter + 1
+        WHERE NodeId = @NodeId
+    END
+    ELSE
+    BEGIN
+        INSERT INTO ReflectionsUmbracoUtilitiesPageViewCounter(NodeId, ViewsCounter) VALUES (@NodeId, 1)
+    END
+END
+GO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+SET ANSI_NULLS ON
+
+GO
+
+SET QUOTED_IDENTIFIER ON
+
+GO
+
+CREATE PROCEDURE [dbo].[ReflectionsUmbracoUtilitiesGetPageViewCount]
+(
+    @NodeId INT
+)
+AS
+BEGIN
+
+    SET NOCOUNT ON;
+
+    SELECT ViewsCounter
+    FROM ReflectionsUmbracoUtilitiesPageViewCounter
+    WHERE NodeId = @NodeId  
+
+END
+
+GO
